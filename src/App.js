8eedf00174d85fe31cfac0a50f04dbe0b0dee190
francisco <franciscojplaras@live.com>
2019-12-10 19:52:00 -0500

    handle change arrow function

--- src/App.js
+++ src/App.js
@@ -2,59 +2,71 @@ import React, { Component } from 'react';
 import './App.css';
 import { CardList } from './components/card-list/card-list.component';
 import { Container, Row, Col } from 'react-bootstrap';
+import SearchBox from './components/search-box/search-box.component';
 
 class App extends Component {
-  constructor(){
+
+  //runs first
+  constructor() {
     super();//calls the constructor method of the component class
-    
+
     this.state = {
       monsters: [],
       searchField: ''
     };//End of state
 
+    //this.handleChange = this.handleChange.bind(this);
+
   } //End of constructor
 
-  componentDidMount(){
+  componentDidMount() {
     //fetch only reject on network failure or if anything prevented the request from completing
     //fetch won't receive cross-site cookies, you can’t establish a cross site session
     //fetch won’t send cookies unless you set the credentials init option
     fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => this.setState({monsters: users}))
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }))
   } //End of componentDidMount
 
-  render(){
+  /*
+  handleChange(event){
+    this.setState({searchField: event.target.value})
+  }
+  */
+
+  //using ES6 arrow functions
+  //arrow functions allow to set this when handleChange is being defined
+  //where the arrow function was defined
+  //lexical scoping
+  handleChange = (event) => {
+    this.setState({ searchField: event.target.value })
+  }
+
+  render() {
 
     //Desctructuring
     //Pull property out of an object and set them to properties
     //This is used to maintain the original datatset
-    const { monsters, searchField} = this.state;
-
+    const { monsters, searchField } = this.state;
     //Filter
     //Includes
     const filteredMonsters = monsters.filter(monster => (
       monster.name.toLowerCase().includes(searchField.toLocaleLowerCase())
     ))
+
     return (
       <div className='App'>
         <Row className='p-2'>
           <Container>
-          <Col>
-            <input type='text' placeholder='Search Monsters' 
-            onChange={ 
-              event => 
-              {
-                //set state is asynchronous, use the callback function to trigger further action after se state has finished
-                //everytime you call set state it re renders, should not have setstate in a render
-                this.setState({ searchField: event.target.value});
-              }
-            } 
-            >
-            </input>
-          </Col>
+            <Col>
+              <SearchBox
+                placeholder='search monsters'
+                handleChange={this.handleChange}>
+              </SearchBox>
+            </Col>
           </Container>
         </Row>
-        <CardList monsters={ filteredMonsters}></CardList>
+        <CardList monsters={filteredMonsters}></CardList>
       </div>
     ); //End of return
   } //End of render
---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-expressions */
+//Functional component
+//Whn you dont need state and life cycle methods create a functional component
+
+import React from 'react';
+
+const SearchBox = ({ placeholder, handleChange}) => {
+    return(
+    <input 
+    type='search' 
+    placeholder={placeholder}
+    onChange={handleChange}>
+    </input>
+    )
+};
+
+export default SearchBox;
