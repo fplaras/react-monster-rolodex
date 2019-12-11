529eed368e56c552da9a01bbf706aae7744add33
fplaras <1815587@fplaras@users.noreply.github.com>
2019-12-10 21:30:29 -0500

    Updates

---
+++ precache-manifest.faae1144cd3852327ec7d39d0aeadf73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acc5a19680c5e9148a0a6a0cc59705ad",
+    "url": "/react-monster-rolodex/index.html"
+  },
+  {
+    "revision": "0cfae35a191a3ee08bac",
+    "url": "/react-monster-rolodex/static/css/main.182d4cdf.chunk.css"
+  },
+  {
+    "revision": "fe002722fd2e0ce2c28c",
+    "url": "/react-monster-rolodex/static/js/2.35d1f360.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/react-monster-rolodex/static/js/2.35d1f360.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0cfae35a191a3ee08bac",
+    "url": "/react-monster-rolodex/static/js/main.292e193f.chunk.js"
+  },
+  {
+    "revision": "1b665022c3deb3ca093a",
+    "url": "/react-monster-rolodex/static/js/runtime-main.298853bc.js"
+  }
+]);
