2c4a88c9e5a765bbeede47de9c9e26f9a92ac7bb
fplaras <1815587@fplaras@users.noreply.github.com>
2019-12-29 14:10:12 -0500

    changed layout of monsters

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,34 +1,30 @@
 import React from 'react';
-import './card-list.styles.css';
+// import './card-list.styles.css';
 import 'react-bootstrap';
-import { Card, Container, Row, CardColumns } from 'react-bootstrap';
+import { Card, Container, Row, Col, CardGroup } from 'react-bootstrap';
 
 //props include a property called children that is anything in between html tags
 export const CardList = (props) => {
     return (
-        /*<div className='card-list'>{props.children}</div>*/
-        /*<div className='card-list'>
-            {
-                props.monsters.map(monster => (
-                <h1 key={ monster.id }> { monster.name } </h1>
-                ))
-            }
-        </div>*/
         <Row>
             <Container>
-                <CardColumns>
                 {
-                    props.monsters.map(monster => (
-                        <Card key={monster.id} className='shadow bg-light'>
-                            <Card.Img variant='top' src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}></Card.Img>
-                            <Card.Header><b>{monster.name}</b></Card.Header>
-                            <Card.Body>
-                                <Card.Text>{monster.email.toLowerCase()}</Card.Text>
-                            </Card.Body>
-                        </Card>
-                    ))
+                    <CardGroup>
+                        {
+                            props.monsters.map(monster => (
+                                <Col sm={3} key={monster.id} className='p-2'>
+                                    <Card key={monster.id} className='shadow bg-light'>
+                                        <Card.Img variant='top' src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}></Card.Img>
+                                        <Card.Header><b>{monster.name} {props.monsters.indexOf(monster)}</b></Card.Header>
+                                        <Card.Body>
+                                            <Card.Text>{monster.email.toLowerCase()}</Card.Text>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                            ))
+                        }
+                    </CardGroup>
                 }
-                </CardColumns>
             </Container>
         </Row>
     );
